48a5d443225a98a79a647c746f116666eca37c20
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-03-14 10:50:15 +0100

    Nueva rama

---
+++ src/main/java/com/bosonit/backend/itinerariocontinuacion/ItinerarioContinuacionApplication.java
@@ -0,0 +1,15 @@
+package com.bosonit.backend.itinerariocontinuacion;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ItinerarioContinuacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItinerarioContinuacionApplication.class, args);
+        log.info("Prueba");
+    }
+}
