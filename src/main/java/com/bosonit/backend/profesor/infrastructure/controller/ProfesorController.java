ccf3d8d8e0e4194df815704d4803cd875544213d
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-03-18 09:00:01 +0100

    CRUD profesor terminado (sin relaciones)

--- src/main/java/com/bosonit/backend/profesor/infrastructure/controller/ProfesorController.java
+++ src/main/java/com/bosonit/backend/profesor/infrastructure/controller/ProfesorController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 public class ProfesorController {
@@ -28,6 +30,12 @@ public class ProfesorController {
         }
     }
 
+    @GetMapping("profesores")
+    @ResponseStatus(HttpStatus.OK)
+    private List<ProfesorOutputDTO> getProfesores() {
+        return service.getProfesores();
+    }
+
     @PostMapping("profesor")
     @ResponseStatus(HttpStatus.CREATED)
     private ResponseEntity<ProfesorOutputDTO> addProfesor(@RequestBody ProfesorInputDTO profesorInputDTO) {
@@ -39,4 +47,29 @@ public class ProfesorController {
         }
     }
 
+    @PutMapping("profesor/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private ResponseEntity<ProfesorOutputDTO> actProfesor(
+            @PathVariable String id,
+            @RequestBody ProfesorInputDTO
+                    profesorInputDTO) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(service.actProfesor(id, profesorInputDTO));
+        } catch (ProfesorNoEncontrado e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error", e);
+        } catch (UnprocesableException u) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "error", u);
+        }
+    }
+
+    @DeleteMapping("profesor/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    private ResponseEntity<Void> delProfesor(@PathVariable String id) {
+        try {
+            service.delProfesor(id);
+            return ResponseEntity.ok().build();
+        } catch (ProfesorNoEncontrado e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error", e);
+        }
+    }
 }
--- src/main/java/com/bosonit/backend/profesor/service/ProfesorServiceImpl.java
+++ src/main/java/com/bosonit/backend/profesor/service/ProfesorServiceImpl.java
@@ -1,12 +1,14 @@
 package com.bosonit.backend.profesor.service;
 
 import com.bosonit.backend.estudiante.infrastructure.controller.dto.EstudianteOutputDTO;
+import com.bosonit.backend.profesor.domain.Profesor;
 import com.bosonit.backend.profesor.infrastructure.controller.dto.ProfesorInputDTO;
 import com.bosonit.backend.profesor.infrastructure.controller.dto.ProfesorOutputDTO;
 import com.bosonit.backend.profesor.infrastructure.controller.mapper.ProfesorMapper;
 import com.bosonit.backend.profesor.repository.ProfesorRepositoryJPA;
 import com.bosonit.backend.utils.exceptions.ProfesorNoEncontrado;
 import com.bosonit.backend.utils.exceptions.UnprocesableException;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -44,16 +46,25 @@ public class ProfesorServiceImpl implements ProfesorService {
 
     @Override
     public ProfesorOutputDTO actProfesor(String id, ProfesorInputDTO profesorInputDTO) {
-        return null;
+        try {
+            Profesor profesor = repository.findById(id)
+                    .orElseThrow(() -> new ProfesorNoEncontrado("Profesor con id: " + id + ", no encontrado"));
+
+            BeanUtils.copyProperties(profesorInputDTO, profesor);
+            return mapper.toDTO(repository.save(profesor));
+        } catch (ConstraintViolationException c) {
+            throw new UnprocesableException(c.getMessage());
+        }
     }
 
     @Override
     public void delProfesor(String id) {
-
+        repository.delete(repository.findById(id)
+                .orElseThrow(() -> new ProfesorNoEncontrado("Profesor con id: " + id + ", no encontrado")));
     }
 
     @Override
     public List<ProfesorOutputDTO> getProfesores() {
-        return null;
+        return mapper.toDTOList(repository.findAll());
     }
 }
