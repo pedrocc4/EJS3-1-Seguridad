d4a30e5d16655eec249420100af0c3df2c079153
Pedro Corpas Cuesta <pedro.corpas@bosonit.com>
2022-04-01 09:21:22 +0200

    Integración de Spring Actuator

--- src/main/java/com/bosonit/backend/EscuelaApp.java
+++ src/main/java/com/bosonit/backend/EscuelaApp.java
@@ -3,6 +3,7 @@ package com.bosonit.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
